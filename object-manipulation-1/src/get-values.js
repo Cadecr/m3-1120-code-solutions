ab8f0ad726d06da39689e1998c02e64000ff0e02
cadecr <dev@Hero-System.localdomain>
2020-11-17 23:44:03 -0800

    object-manip draft2

--- object-manipulation-1/src/get-values.js
+++ object-manipulation-1/src/get-values.js
@@ -1,4 +1,10 @@
 /* eslint-disable no-unused-vars */
 // const getValues = object => {
-
+//   const values = [];
+//   for (var v in object) {
+//     if(object.hasOwnProperty(v)) {
+//       values.push(object[v]);
+//     }
+//   }
+//   return values;
 // }
