031d68603f010a9489f69c192117b9a22d4d8832
cadecr <dev@Hero-System.localdomain>
2020-11-21 13:21:59 -0800

    react-rendering-lists completed

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+
+function PokemonList(props) {
+  const listItems = props.pokedex.map(pokedex =>
+    <li key={pokedex.name}>{pokedex.name}</li>
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+}
+
+ReactDOM.render(
+  <PokemonList pokedex={pokedex} />,
+  document.querySelector('#root')
+);
